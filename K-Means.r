c4c88c8fec6bd5c097f75337a011638be49a11f5
umangU <72771903+umangU@users.noreply.github.com>
2021-04-19 22:14:58 +1000

    Code Updated

--- K-Means.r
+++ K-Means.r
@@ -1,20 +1,20 @@
-#importing the cluster library
+#Importing the cluster library
 library(cluster)
-#reading the snails dataset
+#Reading the snails dataset
 snails <- read_xlsx(file.choose())
-#converting the location variable to numeric
+#Converting the location variable to numeric
 snails$Location=as.numeric(snails$Location)
-#scaling the data
+#Scaling the data
 sd.data=scale(snails)
-#creating variables to store mean silhouette values and the number of clusters respectively
+#Creating variables to store mean silhouette values and the number of clusters respectively
 cluster_ss = vector(,15)
 k_values = vector(,15)
 k.max = 15
 sil <- rep(0, k.max)
-#looping through different values of k
+#Looping through different values of k
 for(i in 2:k.max)
 {
- #k-means algorithm for k=i
+ #K-means algorithm for k=i
  km.out=kmeans(snails,i,nstart=50)
  ss <- silhouette(km.out$cluster, dist(snails))
  sil[i] <- mean(ss[, 3])
@@ -22,8 +22,8 @@ for(i in 2:k.max)
 #Plot the average silhouette width
 plot(1:k.max, sil, type = "b", pch = 19, frame = FALSE, xlab = "Number of clusters k")
 abline(v = which.max(sil), lty = 2)
-#k-means algorithm with the best value of k
+#K-means algorithm with the best value of k
 km.final=kmeans(snails,15,nstart=50)
-#getting within sum of squares
+#Getting within sum of squares
 km.final$tot.withinss
 km.final
