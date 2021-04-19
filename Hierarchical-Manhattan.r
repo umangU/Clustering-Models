d54522f2f0bb8d055af261a4f51dfc612546c827
umangU <72771903+umangU@users.noreply.github.com>
2021-04-19 22:16:30 +1000

    Code Updated

--- Hierarchical-Manhattan.r
+++ Hierarchical-Manhattan.r
@@ -1,16 +1,16 @@
-#reading the snails dataset
+#Reading the snails dataset
 snails <- read_xlsx(file.choose())
-#storing the location variable values
+#Storing the location variable values
 snails_loc <- snails$Location
-#scaling the quantitative data
+#Scaling the quantitative data
 sd.data = scale(snails[2:10])
-#getting the matrix from the scaled data
+#Getting the matrix from the scaled data
 data.dist=dist(sd.data, method="manhattan")
-#hierarchical clustering for 3 different methods
+#Hierarchical clustering for 3 different methods
 hc.complete=hclust(data.dist, method="complete")
-#plot dendograms for all three methods
+#Plot dendograms for all three methods
 plot(hc.complete, main="Complete Linkage", xlab="", sub="",cex=.9)
 abline(h=15, col="red")
-#cutting the tree
+#Cutting the tree
 hc.clusters=cutree(hc.complete,15)
 table(hc.clusters,snails_loc)
