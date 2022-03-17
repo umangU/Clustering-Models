e06c9d1cf46d388a7b20ce305382dc7293a1fcdc
umangU <72771903+umangU@users.noreply.github.com>
2022-03-17 23:10:55 +1100

    Code Updated

--- Hierarchical.r
+++ Hierarchical.r
@@ -4,7 +4,7 @@ snails <- read_xlsx(file.choose())
 snails_loc <- snails$Location
 # Scaling the quantitative data
 sd.data = scale(snails[2:10])
-#Getting the matrix from the scaled data with euclidian dissimilarity measure
+# Getting the matrix from the scaled data with euclidian dissimilarity measure
 data.dist=dist(sd.data)
 # Hierarchical clustering for 3 different methods
 hc.complete=hclust(data.dist, method="complete")
